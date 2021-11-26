77ef0bc01d906fecf5380459f0946e23d22fc357
PeterVermeulen01123 <94803849+PeterVermeulen01123@users.noreply.github.com>
2021-11-26 13:50:42 -0500

    adding the files

---
+++ src/robots.js
@@ -0,0 +1,70 @@
+export const robots =[
+{
+	id: 1,
+	name: 'Sue Denim',
+	username: 'Soo',
+	email: 'Ghost@writer.co'
+},
+
+{
+	id: 2,
+	name: 'Artur Theaph',
+	username: 'Stolen',
+	email: 'art@2go.cya'
+},
+
+{
+	id: 3,
+	name: 'Benji Dover',
+	username: 'Ben',
+	email: 'ben@dover.ch'
+},
+
+{
+	id: 4,
+	name: 'Lee Laa',
+	username: 'Lea',
+	email: 'Lea@graham.gh'
+},
+
+{
+	id: 5,
+	name: 'Will Willoughby',
+	username: '2ea',
+	email: '2ea@graham.gh'
+},
+
+{
+	id: 6,
+	name: 'Sam Samerian',
+	username: '3ea',
+	email: '3ea@graham.gh'
+},
+
+{
+	id: 7,
+	name: 'Geordie LaForge',
+	username: 'Lea',
+	email: 'Lea@graham.gh'
+},
+
+{
+	id: 8,
+	name: 'Captain Kirk',
+	username: '2ea',
+	email: '2ea@graham.gh'
+},
+
+{
+	id: 9,
+	name: 'Annie Sparks',
+	username: '3ea',
+	email: '3ea@graham.gh'
+},
+{
+	id: 10,
+	name: 'Jimmie James',
+	username: '3ea',
+	email: '3ea@graham.gh'
+},
+];
